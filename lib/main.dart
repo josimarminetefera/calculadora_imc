e06b020dd90ecc75d9f926178d50545ac9cd9c5d
josimarminetefera <josimaminete@gmail.com>
2019-01-09 19:26:28 -0200

    Finalizando programa para calcular o imc

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.deepOrange, //cor inicial
+          primaryColor: Color.fromARGB(255, 4, 125, 255)), //Cor principal do aplicativo
+      debugShowCheckedModeBanner: false, //negocio que fica no canto direito
       home: HomeScreen(),
     );
   }
